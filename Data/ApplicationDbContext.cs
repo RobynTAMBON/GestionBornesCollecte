bcb46787c7521340303ec8ff0bbfaf14314e95ea
Tambon <tambonrobynandre@gmail.com>
2026-01-20 19:08:12 +0100

    Ajout de L'ORM

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using GestionBornesCollecte.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionBornesCollecte.Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Benne> Bennes { get; set; }
+        public DbSet<Mesure> Mesures { get; set; }
+    }
+}
