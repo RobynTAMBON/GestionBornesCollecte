bb0dcbbae7f157ae9cd4bf621028eccd4edd552b
Tambon <tambonrobynandre@gmail.com>
2026-01-27 15:05:26 +0100

    Ajout du DTO de l'overview des bennes pour la visualisation sur wpf

---
+++ Dtos/BenneOverviewDto.cs
@@ -0,0 +1,14 @@
+﻿namespace GestionBornesCollecte.Api.Dtos
+{
+    public class BenneOverviewDto
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; } = string.Empty;
+
+        public int? NiveauRemplissage { get; set; }
+        public float? BatterieVolt { get; set; }
+        public DateTime? DerniereMesure { get; set; }
+
+        public string Etat { get; set; } = "INCONNU";
+    }
+}
