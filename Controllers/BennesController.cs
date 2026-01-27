4a98b5f978e173123a75b2b85ccfd9e63d48c119
Tambon <tambonrobynandre@gmail.com>
2026-01-27 03:37:03 +0100

    Ajout du controller des Bennes

---
+++ Controllers/BennesController.cs
@@ -0,0 +1,25 @@
+﻿using GestionBornesCollecte.Api.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestionBornesCollecte.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class BennesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BennesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet(Name = "GetBennes")]
+        public IActionResult GetAll()
+        {
+            var bennes = _context.Bennes.ToList();
+            return Ok(bennes);
+        }
+    }
+}
