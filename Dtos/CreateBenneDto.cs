a60aa3768299f6b245c3004540a397c7d14a2dde
Tambon <tambonrobynandre@gmail.com>
2026-01-27 03:40:14 +0100

    ajout des données du Dto Benne

--- Dtos/CreateBenneDto.cs
+++ Dtos/CreateBenneDto.cs
@@ -1,6 +1,18 @@
-﻿namespace GestionBornesCollecte.Api.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionBornesCollecte.Api.Dtos
 {
     public class CreateBenneDto
     {
+        [Required]
+        [MaxLength(100)]
+        public string Nom { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(200)]
+        public string Localisation { get; set; } = string.Empty;
+
+        [Range(1, 10000)]
+        public int CapaciteMax { get; set; }
     }
 }
