02800d7c2b6f797bdb7f559b59d3c8aa1d29d39a
Tambon <tambonrobynandre@gmail.com>
2026-01-27 04:23:15 +0100

    Post Benne

--- Controllers/BennesController.cs
+++ Controllers/BennesController.cs
@@ -1,4 +1,6 @@
 ﻿using GestionBornesCollecte.Api.Data;
+using GestionBornesCollecte.Api.Dtos;
+using GestionBornesCollecte.Api.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GestionBornesCollecte.Api.Controllers
@@ -15,11 +17,42 @@ namespace GestionBornesCollecte.Api.Controllers
             _context = context;
         }
 
+
+        // GET: api/Bennes
+
         [HttpGet(Name = "GetBennes")]
         public IActionResult GetAll()
         {
             var bennes = _context.Bennes.ToList();
             return Ok(bennes);
+        }
+
+
+        // POST: api/Bennes
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateBenneDto dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var benne = new Benne
+            {
+                Nom = dto.Nom,
+                Localisation = dto.Localisation,
+                CapaciteMax = dto.CapaciteMax
+            };
+
+            _context.Bennes.Add(benne);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                nameof(GetAll),
+                new { id = benne.Id },
+                benne
+            );
         }
     }
 }
