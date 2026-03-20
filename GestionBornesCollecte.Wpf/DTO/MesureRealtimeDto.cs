fa2702645662faafea1d1334d5a4f2a5d1028849
Tambon <tambonrobynandre@gmail.com>
2026-03-20 13:18:50 +0100

    ajout Projet WPF et Client SignalR (webSocket)

---
+++ GestionBornesCollecte.Wpf/DTO/MesureRealtimeDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionBornesCollecte.Wpf.DTO
+{
+    public class MesureRealtimeDto
+    {
+        public int BenneId { get; set; }
+        public int NiveauRemplissage { get; set; }
+        public float BatterieVolt { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ GestionBornesCollecte.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.AspNetCore.SignalR.Client;
+using System.Text.Json;
+using GestionBornesCollecte.Wpf.DTO;
+
+namespace GestionBornesCollecte.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private HubConnection _connection;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            _connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5211/hubs/bennes")   
+                .WithAutomaticReconnect()
+                .Build();
+
+            _connection.On<string>("MesureRecue", json =>
+            {
+                var data = JsonSerializer.Deserialize<MesureRealtimeDto>(json);
+
+                Dispatcher.Invoke(() =>
+                {
+                    MesuresList.Items.Add(
+                        $"Benne {data.BenneId} - {data.NiveauRemplissage}% - {data.BatterieVolt}V"
+                    );
+                });
+            }); ;
+
+            try
+            {
+                await _connection.StartAsync();
+                MesuresList.Items.Add("Connecté à SignalR");
+            }
+            catch (Exception ex)
+            {
+                MesuresList.Items.Add("Erreur : " + ex.Message);
+            }
+        }
+    }
+}
